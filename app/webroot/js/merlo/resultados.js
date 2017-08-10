252a9239fdc03117e66906d95569327176060bbf
Federico Bouzas <federicobouzas@gmail.com>
2017-08-10 13:55:21 -0300

    no message

--- app/webroot/js/merlo/resultados.js
+++ app/webroot/js/merlo/resultados.js
@@ -7,7 +7,7 @@ var agrupaciones = [
     [301, "PATRIA GRANDE", ["VAMOS", "FUERZA DE LA GENTE"]],
     [309, "MOVIMIENTO ORGANIZACION DEMOCRATICA", ["COMPROMISO CIUDADANO"]],
     [501, "1PAIS", ["UN PAIS UNIDO HOY Y SIEMPRE"]],
-    [502, "FRENTE SOCIALISTA Y POPULAR",["LISTA A"]],
+    [502, "FRENTE SOCIALISTA Y POPULAR", ["LISTA A"]],
     ["502/86", "PARTIDO SOCIALISTA", ["FRENTE SOCIALISTA Y POPULAR", "SOCIALISMO MERLENSE"]],
     [503, "UNIDAD CIUDADANA", ["CELESTE Y BLANCA U", "CELESTE Y BLANCA U2"]],
     [504, "IZQUIERDA AL FRENTE POR EL SOCIALISMO", ["UNIDAD DE LA IZQUIERDA"]],
@@ -25,19 +25,30 @@ var agrupaciones = [
 $(function () {
     var linea = 0;
     for (var i in agrupaciones) {
+        var classPrimero = "primero";
         for (var j in agrupaciones[i][2]) {
             var html = '';
-            html += '<div class="col-sm-1 readonly">' + agrupaciones[i][0] + '</div>';
-            html += '<div class="col-sm-4 readonly">' + agrupaciones[i][1] + '</div>';
-            html += '<div class="col-sm-3 readonly">LISTA ' + agrupaciones[i][2][j] + '</div>';
+            html += '<div class="col-sm-1 readonly ' + classPrimero + '">' + agrupaciones[i][0] + '</div>';
+            html += '<div class="col-sm-4 readonly ' + classPrimero + '">' + agrupaciones[i][1] + '</div>';
+            html += '<div class="col-sm-3 readonly ' + classPrimero + '">LISTA ' + agrupaciones[i][2][j] + '</div>';
             var selector = "#agrupaciones .form-group div:nth-child(" + (linea + 1) + ")";
             linea = linea + 7;
-            console.log(agrupaciones[i]);
             $(selector).before(html);
+            classPrimero = "";
         }
     }
 
     $(".disabled").attr("disabled", true).attr("placeholder", "NO USAR");
     $("#agrupaciones .input label").remove();
     $("#agrupaciones div.input").addClass("col-sm-1");
+
+    var html = '';
+    html += '<div class="col-sm-1 cabecera">Nº</div>';
+    html += '<div class="col-sm-4 cabecera">AGRUPACIONES POLÍTICAS</div>';
+    html += '<div class="col-sm-3 cabecera">LISTAS INTERNAS</div>';
+    html += '<div class="col-sm-1 cabecera">SENADORES NACIONALES</div>';
+    html += '<div class="col-sm-1 cabecera">DIPUTADOS NACIONALES</div>';
+    html += '<div class="col-sm-1 cabecera">LEGISLADORES PROVINCIALES</div>';
+    html += '<div class="col-sm-1 cabecera">CONCEJALES Y CONSEJEROS ESCOLARES</div>';
+    $("#agrupaciones .form-group").prepend(html);
 });
