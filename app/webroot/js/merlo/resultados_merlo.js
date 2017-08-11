a36b76952fdb4932600ed3dbfa40ed8c1dbb819d
nicpas <nicpas@gmail.com>
2017-08-11 15:17:26 -0300

    no message

--- app/webroot/js/merlo/resultados_merlo.js
+++ app/webroot/js/merlo/resultados_merlo.js
@@ -29,8 +29,10 @@ function totales() {
 
 function completar_resultados(categoria) {
     $.get(WWW + "merlo/resultados_merlo/ajax_get_resultados_colegios/" + categoria, function (data) {
-        $("#tablaColegios tbody").empty();
+        $("#tablaColegios_" + categoria + " tbody").empty();
+        
         var jdata = $.parseJSON(data);
+        console.log(jdata);
         for (var i in jdata) {
             var row = $("<tr>");
             $("<td>").html("<strong>" + jdata[i].ResultadoMerlo.establecimiento + "</strong>").appendTo(row);
@@ -38,20 +40,21 @@ function completar_resultados(categoria) {
             $("<td class='text-center'>").text(jdata[i][0]['503_' + categoria + ''] ? number_format(jdata[i][0]['503_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
             $("<td class='text-center'>").text(jdata[i][0]['508_' + categoria + ''] ? number_format(jdata[i][0]['508_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
             $("<td class='text-center'>").text(jdata[i][0]['509_' + categoria + ''] ? number_format(jdata[i][0]['509_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
-            $("#tablaColegios tbody").append(row);
+            $("#tablaColegios_" + categoria + " tbody").append(row);
         }
     });
     $.get(WWW + "merlo/resultados_merlo/ajax_get_resultados_circuitos/" + categoria, function (data) {
-        $("#tablaCircuitos tbody").empty();
+        $("#tablaCircuitos_" + categoria + " tbody").empty();
         var jdata = $.parseJSON(data);
+        console.log(jdata);
         for (var i in jdata) {
             var row = $("<tr>");
             $("<td>").html("<strong>" + jdata[i].ResultadoMerlo.circuito + "</strong>").appendTo(row);
-            $("<td class='text-center'>").text(jdata[i][0]['501_' + categoria + ''] ? number_format(jdata[i][0]['501_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
-            $("<td class='text-center'>").text(jdata[i][0]['503_' + categoria + ''] ? number_format(jdata[i][0]['503_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
-            $("<td class='text-center'>").text(jdata[i][0]['508_' + categoria + ''] ? number_format(jdata[i][0]['508_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
-            $("<td class='text-center'>").text(jdata[i][0]['509_' + categoria + ''] ? number_format(jdata[i][0]['509_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
-            $("#tablaCircuitos tbody").append(row);
+            $("<td class='text-center'>").text(jdata[i].ResultadoMerlo['501_' + categoria + ''] ? number_format(jdata[i][0]['501_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
+            $("<td class='text-center'>").text(jdata[i].ResultadoMerlo['503_' + categoria + ''] ? number_format(jdata[i][0]['503_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
+            $("<td class='text-center'>").text(jdata[i].ResultadoMerlo['508_' + categoria + ''] ? number_format(jdata[i][0]['508_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
+            $("<td class='text-center'>").text(jdata[i].ResultadoMerlo['509_' + categoria + ''] ? number_format(jdata[i][0]['509_' + categoria + ''], 2, ",") + "%" : "").appendTo(row);
+            $("#tablaCircuitos_" + categoria + " tbody").append(row);
         }
     });
 }
