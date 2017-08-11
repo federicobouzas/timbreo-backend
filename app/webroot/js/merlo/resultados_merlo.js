dc0dd814d284839493da5dd31d28a8ede3a726e0
Federico Bouzas <federicobouzas@gmail.com>
2017-08-11 12:26:28 -0300

    no message

--- app/webroot/js/merlo/resultados_merlo.js
+++ app/webroot/js/merlo/resultados_merlo.js
@@ -11,7 +11,14 @@ $(function () {
 });
 
 function totales() {
-
+    grafico_dona("dona-senadores");
+    grafico_barras("barras-senadores");
+    grafico_dona("dona-diputados");
+    grafico_barras("barras-diputados");
+    grafico_dona("dona-legisladores");
+    grafico_barras("barras-legisladores");
+    grafico_dona("dona-concejales");
+    grafico_barras("barras-concejales");
 }
 
 function senadores() {
@@ -38,5 +45,60 @@ function senadores() {
             $("<td class='text-center'>").text(jdata[i][0]['509_sen'] ? number_format(jdata[i][0]['509_sen'], 2, ",") + "%" : "").appendTo(row);
             $("#tablaCircuitos tbody").append(row);
         }
+    });
+}
+
+function grafico_dona(container, title) {
+    $("[id='" + container + "']").highcharts({
+        chart: {
+            margin: [0, 0, 0, 0], spacingTop: 0, spacingBottom: 0, spacingLeft: 0, spacingRight: 0,
+            plotBackgroundColor: null, plotBorderWidth: 0, plotShadow: false, style: {fontFamily: 'Gotham'}
+        },
+        colors: ['#F44336', '#037DBF', '#FFD300', '#A655AC'],
+        exporting: {enabled: false},
+        credits: {enabled: false},
+        title: {text: title},
+        tooltip: {enabled: false},
+        plotOptions: {
+            pie: {
+                size: '120%', dataLabels: {enabled: false}, startAngle: -90, endAngle: 90, center: ['50%', '65%']
+            }
+        },
+        series: [{
+                type: 'pie',
+                name: 'Votos',
+                innerSize: '50%',
+                data: [['1PAIS', 10.38], ['UNIDAD CIUDADANA', 56.33], ['CAMBIEMOS', 24.03], ['PJ', 4.77]]
+            }]
+    });
+}
+
+function grafico_barras(container) {
+    $("[id='" + container + "']").highcharts({
+        chart: {type: 'bar', style: {fontFamily: 'Gotham'}},
+        exporting: {enabled: false},
+        credits: {enabled: false},
+        title: {text: null},
+        xAxis: {categories: ['1PAIS', 'UC', 'PRO', 'PJ'], title: {text: null}},
+        yAxis: {min: 0, title: {text: null}, labels: {enabled: false}},
+        tooltip: {enabled: false},
+        plotOptions: {bar: {stacking: 'percent', dataLabels: {enabled: false}}},
+        legend: {enabled: false},
+        series: [
+            {name: 'Faltante', data: [
+                    {y: 341, color: '#CCC'},
+                    {y: 773, color: '#CCC'},
+                    {y: 869, color: '#CCC'},
+                    {y: 945, color: '#CCC'}
+                ]
+            },
+            {name: 'Votos', data: [
+                    {y: 635, color: '#FFD300'},
+                    {y: 203, color: '#A655AC'},
+                    {y: 107, color: '#F44336'},
+                    {y: 31, color: '#037DBF'}
+                ]
+            }
+        ]
     });
 }
